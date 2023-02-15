9242cd3efaf38c8b7479950e0545cd5b0cc9ff47
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2023-02-15 22:44:40 +0300

    final

--- RezantsevaAA/Practice2_0/Practice2_0/main.c
+++ RezantsevaAA/Practice2_0/Practice2_0/main.c
@@ -1,40 +1,46 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "matrix.h"
 #include <stdio.h>
+#include "matrix.h"
+
+
 int main()
 {
-	TMatrix* matrix_dynamic, *m1, *m2, *res;
-/*
-	allocate_matrix(&matrix_dynamic, 2);
-	fill_matrix(matrix_dynamic);
-	print_matrix(matrix_dynamic);
-	free_matrix(&matrix_dynamic);
-*/
-	allocate_matrix(&m1, 2);
-	allocate_matrix(&m2, 2);
-	fill_matrix(m1);
-	fill_matrix(m2);
+    TMatrix* matrix_dynamic, * m1, * m2, * res;
+   /*
+    allocate_matrix(&matrix_dynamic, 2);
+    fill_matrix(matrix_dynamic);
+    print_matrix(matrix_dynamic);
+    free_matrix(&matrix_dynamic);
+    */
 
-	print_matrix(m1);
-	print_matrix(m2);
+    allocate_matrix(&m1, 2);
+    allocate_matrix(&m2, 2);
+   
+    fill_matrix(m1);
+    fill_matrix(m2);
+   
+    res = add_matrix(m1, m2);
+    print_matrix(res);
+    free_matrix(&res);
+   
+    res = add_const(m1, 2);
+    print_matrix(res);
+    free_matrix(&res);
 
-	res = add_matrix(m1, m2);
-	print_matrix(res);
-	free_matrix(&res);
+    res = add_const(m2, 2);
+    print_matrix(res);
+    free_matrix(&res);
+   
+    res = multi_const(m1, 2);
+    print_matrix(res);
+    free_matrix(&res);
 
-	res = add_const(m1, 2);
-	print_matrix(res);
-	free_matrix(&res);
-
-	res = multi_matrix(m1, 2);
-	print_matrix(res);
-	free_matrix(&res);
-
-	res = multi_const(m1, 2);
-	print_matrix(res);
-	free_matrix(&res);
-
-	free_matrix(&m1);
-	free_matrix(&m1);
-	return 0;
+    res = multi_const(m2, 2);
+    print_matrix(res);
+    free_matrix(&res);
+   
+    res = multi_matrix(m1, m2);
+    print_matrix(res);
+    free_matrix(&res);
+   
+    return 0;
 }
--- RezantsevaAA/Practice2_0/Practice2_0/matrix.h
+++ RezantsevaAA/Practice2_0/Practice2_0/matrix.h
@@ -1,11 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
 #ifndef _MATRIX_H
 #define _MATRIX_H
 typedef struct
 {
 	float* x;
 	int n;
-}TMatrix;
+} TMatrix;
 
 void allocate_matrix(TMatrix** matrix, int n);
 void free_matrix(TMatrix** matrix);
@@ -18,4 +17,4 @@ TMatrix* add_const(TMatrix* matrix, float c);
 
 TMatrix* multi_matrix(TMatrix* matrix1, TMatrix* matrix2);
 TMatrix* multi_const(TMatrix* matrix, float c);
-#endif  _MATRIX_H
+#endif _MATRIX_H
