c44e58d112b660946a11402da90729486ef76edf
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2023-02-15 22:52:10 +0300

    final version

--- RezantsevaAA/Practice2_0/Practice2_0/main.c
+++ RezantsevaAA/Practice2_0/Practice2_0/main.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include "matrix.h"
-
-
 int main()
 {
     TMatrix* matrix_dynamic, * m1, * m2, * res;
-   /*
+    /*
     allocate_matrix(&matrix_dynamic, 2);
     fill_matrix(matrix_dynamic);
     print_matrix(matrix_dynamic);
     free_matrix(&matrix_dynamic);
     */
-
     allocate_matrix(&m1, 2);
     allocate_matrix(&m2, 2);
    
@@ -41,6 +38,8 @@ int main()
     res = multi_matrix(m1, m2);
     print_matrix(res);
     free_matrix(&res);
-   
+
+    free_matrix(&m1);
+    free_matrix(&m2);
     return 0;
 }
